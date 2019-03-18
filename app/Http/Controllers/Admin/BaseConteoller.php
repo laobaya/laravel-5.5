91156affef69c199af656e6f4038393f3cd5a253
laobaya <35564082+laobaya@users.noreply.github.com>
2019-03-18 17:33:06 +0800

    更新

---
+++ app/Http/Controllers/Admin/BaseConteoller.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+class BaseController extends Controller
+{
+    
+    //视图主目录
+    protected $ViewPath = 'admin';
+
+    //视图路径
+    protected $Path;
+
+
+}
